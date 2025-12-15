57ba9f8ea598486de60c9a169008a44e1d355e0e
sameerdewan <sameer@lunarlaunchlabs.com>
2025-12-14 20:44:39 -0500

    (chore) Clerk publishable key

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
 import { WalletProvider } from "./providers/WalletProvider";
+import { CLERK_PUBLISHABLE_KEY } from "./services/ClerkService";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <WalletProvider>{children}</WalletProvider>
